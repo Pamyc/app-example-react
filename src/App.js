0874c6017e5e7ea432e5e99e4dee12156fc44f98
Pamyc <163673087+Pamyc@users.noreply.github.com>
2025-10-06 10:19:58 +0300

    Format greeting placement

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {' привет эрик'}
         </p>
         <a
           className="App-link"
